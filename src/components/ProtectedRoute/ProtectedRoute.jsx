1b65c248c620e23f18d0db99cbf483e2e6cc8f50
brodilli <brodilli_@hotmail.com>
2023-10-29 17:02:43 -0600

    prueba de rutas

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import React from "react";
 export const ProtectedRoute = ({ login, children }) => {
   if (!login) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/" />;
   }
   return <Outlet />;
 };
